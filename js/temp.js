f4df15be3177c4de3ff9824233ca291072ebdab6
petrolingus <petrolingus@gmail.com>
2021-03-26 22:58:30 +0300

    disabled outline and little fixes in temp.js

--- js/temp.js
+++ js/temp.js
@@ -1,8 +1,10 @@
-function resizable (el, factor) {
-  var int = Number(factor) || 7.7;
-  function resize() {el.style.width = ((el.value.length+1) * int) + 'px'}
-  var e = 'keyup,keypress,focus,blur,change'.split(',');
-  for (var i in e) el.addEventListener(e[i],resize,false);
-  resize();
-}
-resizable(document.getElementById('txt'), 48);
+document.getElementById("editor1").addEventListener("input", function() {
+    console.log("input event fired");
+    this.innerHTML = "0";
+}, false);
+
+document.getElementById("editor2").addEventListener("input", function() {
+    let text = this.innerHTML;
+    let edited = text.replace(/[A-Z]/g, '');
+    this.innerHTML = '0';
+}, false);
