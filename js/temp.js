45aca86fb4b1c4a6b396ac41883cafe4a777cf1b
petrolingus <petrolingus@gmail.com>
2021-03-31 19:35:52 +0300

    some fixes

--- js/temp.js
+++ js/temp.js
@@ -5,15 +5,21 @@ document.getElementById("editor1").addEventListener("input", function() {
 
 document.getElementById("editor2").addEventListener("input", function() {
     let text = this.innerHTML;
-    let edited = text.replace(/[A-Z]/g, '');
-    this.innerHTML = '0';
+    let edited = text.replace(/\D/g, '');
+    this.innerHTML = edited;
 }, false);
 
 let tbl = document.getElementById("mytable");
 let tbody = tbl.children[2];
 console.log(tbody);
 
-for (var i = 0; i < 100; ++i) {
+function foo() {
+  let newRow = document.createElement("tr");
+  newRow.innerHTML = "<td>" + 1 + "</td><td>4</td><td>2</td><td>144</td><td>0</td><td>0</td>";
+  tbody.insertBefore(newRow, tbody.firstChildren);
+}
+
+for (var i = 0; i < 1; ++i) {
   let newRow = document.createElement("tr");
   newRow.innerHTML = "<td>" + i + "</td><td>4</td><td>2</td><td>144</td><td>0</td><td>0</td>";
   tbody.insertBefore(newRow, tbody.firstChildren);
