b704c712f585644f5949f9cd5da4fc562fd4cdc9
petrolingus <petrolingus@gmail.com>
2021-03-31 13:38:48 +0300

    some fixes in all temp files

--- js/temp.js
+++ js/temp.js
@@ -8,3 +8,13 @@ document.getElementById("editor2").addEventListener("input", function() {
     let edited = text.replace(/[A-Z]/g, '');
     this.innerHTML = '0';
 }, false);
+
+let tbl = document.getElementById("mytable");
+let tbody = tbl.children[2];
+console.log(tbody);
+
+for (var i = 0; i < 100; ++i) {
+  let newRow = document.createElement("tr");
+  newRow.innerHTML = "<td>" + i + "</td><td>4</td><td>2</td><td>144</td><td>0</td><td>0</td>";
+  tbody.insertBefore(newRow, tbody.firstChildren);
+}
